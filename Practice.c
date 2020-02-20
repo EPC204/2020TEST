7cdae4661b0f01cf961c1587c99728b01a726998
xuyb <yinbiao_Xu@126.com>
2020-02-21 01:10:33 +0800

    第一次提交的文件!

---
+++ Practice.c
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2020-01-30 14:16:54
+ * @LastEditTime : 2020-01-30 14:25:04
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \C\Practice.c
+ */
+#include <stdio.h>
+int main ()
+{
+    printf("hello,world!");
+    return 0;
+}
